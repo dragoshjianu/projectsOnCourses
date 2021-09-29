521b0fd9aeb4062ced6863505d916e48e3598745
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2021-09-29 14:02:29 +0300

    update js course

--- async/app.js
+++ async/app.js
@@ -19,28 +19,99 @@
 
 // getPlanets();
 
-const moveX = (element, amount, delay) => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			const bodyBoundry = document.body.clientWidth;
-			const elRight = element.getBoundingClientRect().right;
-			const currLeft = element.getBoundingClientRect().left;
+// Multiple Awaits
+// const moveX = (element, amount, delay) => {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			const bodyBoundry = document.body.clientWidth;
+// 			const elRight = element.getBoundingClientRect().right;
+// 			const currLeft = element.getBoundingClientRect().left;
 
-			if (elRight + amount > bodyBoundry) {
-				reject({ bodyBoundry, elRight, amount });
-			} else {
-				element.style.transform = `translateX(${currLeft + amount}px)`;
-				resolve();
-			}
-		}, delay);
-	});
-};
+// 			if (elRight + amount > bodyBoundry) {
+// 				reject({ bodyBoundry, elRight, amount });
+// 			} else {
+// 				element.style.transform = `translateX(${currLeft + amount}px)`;
+// 				resolve();
+// 			}
+// 		}, delay);
+// 	});
+// };
 
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-async function animateBtn(el) {
-	await moveX(el, 100, 1000);
-	moveX(el, 100, 1000);
+// async function animateBtn(el, amt) {
+// 	await moveX(el, amt, 1000);
+// 	await moveX(el, amt, 1000);
+// 	await moveX(el, amt, 1000);
+// 	await moveX(el, amt, 1000);
+// 	await moveX(el, amt, 1000);
+// 	await moveX(el, amt, 1000);
+// 	await moveX(el, amt, 1000);
+// 	await moveX(el, amt, 1000);
+// }
+
+// animateBtn(btn, 100).catch((err) => {
+// 	console.log("All done!!");
+// 	animateBtn(btn, -100);
+// });
+
+// PARALLEL & SEQUENTIAL REQUESTS
+// //Squential req
+// async function get3Pokemon() {
+// 	const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
+// 	const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
+// 	const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+// 	console.log(poke1.data);
+// 	console.log(poke2.data);
+// 	console.log(poke3.data);
+// }
+
+//Parallel req
+async function get3Pokemon() {
+	const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+	const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+	const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+	const results = await Promise.all([prom1, prom2, prom3]);
+	printPokemonNames(results);
+	console.log(results);
 }
 
-animateBtn(btn);
+function printPokemonNames(results) {
+	results.forEach((pokemon) => {
+		console.log(pokemon.data.name);
+	});
+}
+
+get3Pokemon();
+
+// function changeBodyColor(color, delay) {
+// 	return new Promise((res, rej) => {
+// 		setTimeout(() => {
+// 			document.body.style.backgroundColor = color;
+// 			res();
+// 		}, delay);
+// 	});
+// }
+
+//Sequence
+// async function lightShow() {
+// 	await changeBodyColor("teal", 1000);
+// 	await changeBodyColor("pink", 1000);
+// 	await changeBodyColor("indigo", 1000);
+// 	await changeBodyColor("violet", 1000);
+// }
+
+// //Parallel
+// async function lightShow() {
+// 	const p1 = changeBodyColor("teal", 1000);
+// 	const p2 = changeBodyColor("pink", 1000);
+// 	const p3 = changeBodyColor("indigo", 1000);
+// 	const p4 = changeBodyColor("violet", 1000);
+
+// 	await p1;
+// 	await p2;
+// 	await p3;
+// 	await p4;
+// }
+
+// lightShow();
