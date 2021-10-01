6a6d54b866c6cb550d8b69c03a734441c2703b8c
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2021-10-01 14:30:05 +0300

    timer

--- timer/app.js
+++ timer/app.js
@@ -1,13 +1,30 @@
 console.log("App loaded");
-
 class Timer {
 	constructor(durationInput, startButton, pauseButton) {
 		this.durationInput = durationInput;
 		this.startButton = startButton;
 		this.pauseButton = pauseButton;
+
+		this.startButton.addEventListener("click", this.start);
+		this.pauseButton.addEventListener("click", this.pause);
 	}
 
-	start() {
-		console.log("Start timeer");
-	}
+	start = () => {
+		this.tick();
+		this.intervalId = setInterval(this.tick, 1000);
+	};
+
+	pause = () => {
+		clearInterval(this.intervalId);
+	};
+
+	tick = () => {
+		console.log("tick");
+	};
 }
+
+const durationInput = document.querySelector("#duration");
+const startBtn = document.querySelector("#start");
+const pauseBtn = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
