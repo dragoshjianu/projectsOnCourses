d722aad4cd103c66dfa36fcb64cbc9c3de0c54d3
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2021-10-18 15:51:53 +0300

    API init

---
+++ climate-change-api/index.js
@@ -0,0 +1,15 @@
+const PORT = 8000;
+
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.json("Welcome to Climate Changes");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
