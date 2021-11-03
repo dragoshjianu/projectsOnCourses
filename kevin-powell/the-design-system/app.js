990817e07453b49e214484898ac93c09c808f3bc
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2021-11-03 10:23:22 +0200

    update

---
+++ kevin-powell/the-design-system/app.js
@@ -0,0 +1,18 @@
+// javascript
+
+const toggleBtn = document.querySelector('.mobile-nav-toggle');
+const nav = document.querySelector('.primary-navigation');
+
+toggleBtn.addEventListener('click', () => {
+  const visible = nav.getAttribute('data-visible')
+  if (visible === 'false') {
+    nav.setAttribute('data-visible', true);
+    toggleBtn.setAttribute('aria-expanded', true);
+  };
+  if (visible === 'true') {
+    nav.setAttribute('data-visible', false);
+    toggleBtn.setAttribute('aria-expanded', false);
+  }
+
+  console.log(toggleBtn.getAttribute('aria-expanded'));
+})
