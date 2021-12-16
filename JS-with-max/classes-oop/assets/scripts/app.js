6a172b20c46d41be6544253c8f329fe6656f0c15
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2021-12-16 16:05:00 +0200

    Update app.js

--- JS-with-max/classes-oop/assets/scripts/app.js
+++ JS-with-max/classes-oop/assets/scripts/app.js
@@ -1,12 +1,35 @@
-divconst;
+class Product {
+	title = "DEFAULT";
+	imageUrl;
+	description;
+	price;
+
+	constructor(title, image, description, price) {
+		this.title = title;
+		this.imageUrl = image;
+		this.description = description;
+		this.price = price;
+	}
+}
 
 const productList = {
 	products: [
-		{ title: "A Pillow", imageUrl: "#", price: "19.99", description: "A soft pillow" },
-		{ title: "A Carpet", imageUrl: "#", price: "89.99", description: "A carpet which you might like" },
+		new Product(
+			"A Pillow",
+			"https://st.hzcdn.com/simgs/89912b1602ab900a_3-9667/contemporary-decorative-pillows.jpg",
+			"A soft pillow",
+			29.99
+		),
+
+		{
+			title: "A Carpet",
+			imageUrl: "https://st.hzcdn.com/simgs/6a5162c80b1ee5e0_3-0605/contemporary-area-rugs.jpg",
+			price: 89.99,
+			description: "A carpet which you might like",
+		},
 	],
 	render() {
-		const renderHook = document.querySelector("#app");
+		const renderHook = document.getElementById("app");
 		const prodList = document.createElement("ul");
 		prodList.className = "product-list";
 		for (const prod of this.products) {
@@ -14,13 +37,19 @@ const productList = {
 			prodEl.className = "product-item";
 			prodEl.innerHTML = `
 				<div>
-					<img src="${prod.imageUrl}" alt="${prod.title}"/>
-					<div class="product-item__content>
-						<h2></h2>
+					<img src="${prod.imageUrl}" alt="${prod.title}" />
+					<div class="product-item__content">
+						<h2>${prod.title}</h2>
+						<h3>$${prod.price}</h3>
+						<p>${prod.description}</p>
+						<button>Add to Cart</button>
 					</div>
 				</div>
 			`;
+			prodList.append(prodEl);
 		}
-		renderHook.append();
+		renderHook.append(prodList);
 	},
 };
+
+productList.render();
