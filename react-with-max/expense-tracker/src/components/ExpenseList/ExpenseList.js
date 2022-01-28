5f3d025215875f3411b6c7c33c37649c139d2a39
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-01-29 00:00:47 +0200

    Update ExpenseList.js

--- react-with-max/expense-tracker/src/components/ExpenseList/ExpenseList.js
+++ react-with-max/expense-tracker/src/components/ExpenseList/ExpenseList.js
@@ -6,27 +6,35 @@ import ExpensesFilter from './ExpensesFilter/ExpensesFilter';
 import './ExpenseList.css';
 
 const ExpenseList = (props) => {
-	const [filteredYear, setFilteredYear] = useState('2020');
+	const [filteredYear, setFilteredYear] = useState('2022');
 
 	const filterChangeHandler = (selectedYear) => {
 		console.log('from List');
 		setFilteredYear(selectedYear);
 	};
+
+	const filteredData = props.items.filter(
+		(year) => year.date.getFullYear().toString() === filteredYear
+	);
+
 	return (
 		<div>
 			<Card className='expenses'>
 				<ExpensesFilter selected={filteredYear} onFilterYearData={filterChangeHandler} />
-
-				{props.items.map((expense) => {
-					return (
-						<ExpenseItem
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					);
-				})}
+				{filteredData.lenght === 0 ? (
+					<p>No expensese found</p>
+				) : (
+					filteredData.map((expense) => {
+						return (
+							<ExpenseItem
+								key={expense.id}
+								title={expense.title}
+								amount={expense.amount}
+								date={expense.date}
+							/>
+						);
+					})
+				)}
 			</Card>
 		</div>
 	);
