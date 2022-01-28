a9d50f3999857c2b3c9edafe87c37ff76f5a2a7b
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-01-28 23:33:57 +0200

    update project

--- react-with-max/expense-tracker/src/components/ExpenseList/ExpenseList.js
+++ react-with-max/expense-tracker/src/components/ExpenseList/ExpenseList.js
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem/ExpenseItem';
+import ExpensesFilter from './ExpensesFilter/ExpensesFilter';
 
 import './ExpenseList.css';
 
 const ExpenseList = (props) => {
+	const [filteredYear, setFilteredYear] = useState('2020');
+
+	const filterChangeHandler = (selectedYear) => {
+		console.log('from List');
+		setFilteredYear(selectedYear);
+	};
 	return (
-		<Card className='expenses'>
-			<ExpenseItem
-				title={props.expenses[0].title}
-				amount={props.expenses[0].amount}
-				date={props.expenses[0].date}
-			/>
-			<ExpenseItem
-				title={props.expenses[1].title}
-				amount={props.expenses[1].amount}
-				date={props.expenses[1].date}
-			/>
-			<ExpenseItem
-				title={props.expenses[2].title}
-				amount={props.expenses[2].amount}
-				date={props.expenses[2].date}
-			/>
-			<ExpenseItem
-				title={props.expenses[3].title}
-				amount={props.expenses[3].amount}
-				date={props.expenses[3].date}
-			/>
-		</Card>
+		<div>
+			<Card className='expenses'>
+				<ExpensesFilter selected={filteredYear} onFilterYearData={filterChangeHandler} />
+
+				{props.items.map((expense) => {
+					return (
+						<ExpenseItem
+							key={expense.id}
+							title={expense.title}
+							amount={expense.amount}
+							date={expense.date}
+						/>
+					);
+				})}
+			</Card>
+		</div>
 	);
 };
 
