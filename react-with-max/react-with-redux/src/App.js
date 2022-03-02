c05be50050b8d6ea5f9564ce3574916113e49d23
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-03-02 15:08:09 +0200

    Update

--- react-with-max/react-with-redux/src/App.js
+++ react-with-max/react-with-redux/src/App.js
@@ -2,25 +2,17 @@ import React from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
-import { authActions } from './store/index';
-import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function App() {
-	const dispatch = useDispatch;
-
-	const loginHandler = () => {
-		dispatch(authActions.login());
-	};
-
-	const logoutHandler = () => {
-		dispatch(authActions.logout());
-	};
+	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
 	return (
 		<>
 			<Header />
-			<Auth />
+			{!isAuthenticated ? <Auth /> : <UserProfile />}
 			<Counter />
 		</>
 	);
--- react-with-max/react-with-redux/src/components/Counter.js
+++ react-with-max/react-with-redux/src/components/Counter.js
@@ -1,6 +1,6 @@
 // import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { counterActions } from '../store/index';
+import { counterActions } from '../store/counter';
 
 import classes from './Counter.module.css';
 
@@ -8,8 +8,8 @@ const Counter = () => {
 	// const [showCounter, setShowCounter] = useState(true);
 
 	const dispatch = useDispatch();
-	const counter = useSelector((state) => state.counter);
-	const show = useSelector((state) => state.showCounter);
+	const counter = useSelector((state) => state.counter.counter);
+	const show = useSelector((state) => state.counter.showCounter);
 
 	const incrementHandler = () => {
 		dispatch(counterActions.increment());
