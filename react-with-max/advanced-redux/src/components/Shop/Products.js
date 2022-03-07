51a6d2c2d86e454b4ee08f0f3061ff08f1ce300f
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-03-07 13:38:07 +0200

    update

--- react-with-max/advanced-redux/src/components/Shop/Products.js
+++ react-with-max/advanced-redux/src/components/Shop/Products.js
@@ -1,19 +1,33 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+	{ id: 'p1', title: 'My First Book', price: 6, description: 'the Book I ever wrote' },
+	{
+		id: 'p2',
+		title: 'My Second Book',
+		price: 8,
+		description: 'the second Book I ever wrote',
+	},
+];
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{DUMMY_PRODUCTS.map((product) => (
+					<ProductItem
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
