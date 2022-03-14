0453ddfc9e65bec15c19a12350a35d90479d5802
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-03-14 15:12:51 +0200

    update

--- react-with-max/react-multipage/src/App.js
+++ react-with-max/react-multipage/src/App.js
@@ -1,12 +1,26 @@
+import React from 'react';
+
+import { Route } from 'react-router-dom';
+
+import MainHeader from './components/MainHeader';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+
 function App() {
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<MainHeader />
+			<main>
+				<Route path='/welcome'>
+					<Welcome />
+				</Route>
+				<Route path='/products'>
+					<Products />
+				</Route>
+				<Route path='/product-detail'></Route>
+			</main>
 		</div>
 	);
 }
 
 export default App;
-
-// out-domain.com/ => Component A
-// out-domain.com/products => Component B
