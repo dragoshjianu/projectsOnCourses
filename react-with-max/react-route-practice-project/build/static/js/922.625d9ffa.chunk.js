cf3577e970e337f16a7d9d50b0801d4fd827e1f1
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-03-25 15:57:45 +0200

    update

---
+++ react-with-max/react-route-practice-project/build/static/js/922.625d9ffa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_complete_guide=self.webpackChunkreact_complete_guide||[]).push([[922],{922:function(e,c,n){n.r(c);n(791);var t=n(184);c.default=function(){return(0,t.jsx)("div",{className:"centered",children:(0,t.jsx)("p",{children:"Page not found"})})}}}]);
+//# sourceMappingURL=922.625d9ffa.chunk.js.map
