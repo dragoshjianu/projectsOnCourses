b609335b37719751dd42c5bbb07d97382ea4232f
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-05-02 19:39:20 +0300

    restructure

--- cms_blog/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react'
-
-import { Link } from 'next/Link'
-
-const Header = () => {
-  return <div className="container mx-auto px-10">Header</div>
-}
-
-export default Header
