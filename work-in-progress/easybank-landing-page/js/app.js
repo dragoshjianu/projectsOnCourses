668ccbcc360477871fd4b06a93a819fca5c7f41c
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-05-03 10:40:34 +0300

    update refactor

--- work-in-progress/easybank-landing-page/js/app.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.querySelector(".toggle-menu");
-const overlay = document.querySelector(".overlay");
-const menu = document.querySelector(".menu-list");
-
-toggleBtn.addEventListener("click", () => {
-	toggleBtn.classList.toggle("toggled");
-	menu.classList.toggle("opened");
-	overlay.classList.toggle("opened");
-});
