cd5eff44d55f5ee45e85e137527b75b674ec2041
dragoshjianu <dragosh.jianu@gmail.com>
2022-08-19 09:55:43 +0300

    update

--- tutorial-projects/portfolio/src/App.jsx
+++ tutorial-projects/portfolio/src/App.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import {
-	Header,
-	// About,
-	// Contact,
-	// Experience,
-	// Footer,
-	// Navigation,
-	// Portfolio,
-	// Services,
-	// Testimonials,
+	About,
+	Contact,
+	Experience,
+	Navigation,
+	Portfolio,
+	Services,
+	Testimonials,
 } from './components/sections';
+import { Header, Main, Footer } from './components/Layuout';
 
 const App = () => {
 	return (
 		<div>
 			<>
 				<Header />
-				{/* <Navigation /> */}
+				<Main>
+					<Navigation />
+					<About />
+					<Experience />
+					<Services />
+					<Portfolio />
+					<Testimonials />
+					<Contact />
+				</Main>
+
+				<Footer />
 			</>
 		</div>
 	);
---
+++ tutorial-projects/portfolio/src/components/Layuout/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Main.css';
+
+const Main = (props) => {
+	return <main>{props.children}</main>;
+};
+
+export default Main;
