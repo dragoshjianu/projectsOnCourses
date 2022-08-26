b16ee215ebf8ea5f9bafacc2f787c1b59d72e0b1
dragoshjianu <dragosh.jianu@gmail.com>
2022-08-26 13:47:05 +0300

    update

--- gradinita-static-website/src/assets/js/main.js
+++ gradinita-static-website/src/assets/js/main.js
@@ -1,3 +1,14 @@
+// Functions
+
+const addFaIconToMenu = (icon, parent, location) => {
+	const element = document.createElement('i');
+	element.classList.add('fa-solid', `fa-${icon}`, 'expand');
+	const linkItem = parent.querySelector('a');
+
+	linkItem.insertAdjacentElement(location, element);
+};
+
+// Toggle Menu
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.main-menu');
 
@@ -6,4 +17,19 @@ navToggle.addEventListener('click', () => {
 		? navToggle.setAttribute('aria-expanded', false)
 		: navToggle.setAttribute('aria-expanded', true);
 	primaryNav.toggleAttribute('data-visible');
+});
+
+// Add dropdown icon to parent menu item
+const menuElements = primaryNav.querySelectorAll('.main-menu-item');
+
+menuElements.forEach((element) => {
+	const children = element.children;
+	for (child of children) {
+		if (child.tagName === 'UL') {
+			const parentLi = child.parentElement;
+			parentLi.setAttribute('data-parent', '');
+			console.log(parentLi);
+			addFaIconToMenu('caret-down', parentLi, 'beforeEnd');
+		}
+	}
 });
