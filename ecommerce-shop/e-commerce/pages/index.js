b9a3d3e5bb333ab0a1c691c1d671f266eabb74f1
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-11-17 16:29:35 +0200

    update

---
+++ ecommerce-shop/e-commerce/pages/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = ({ products, bannerData }) => {
+	return (
+		<>
+			<HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+			{console.log('Banner data: ', bannerData)}
+			<div className='products-heading'>
+				<h2>Best Selling Products</h2>
+				<p>Speakers of many variations</p>
+			</div>
+			<div className='products-container'>
+				{products.map((product) => (
+					<Product key={product._id} product={product} />
+				))}
+			</div>
+
+			<FooterBanner footerBanner={bannerData.length && bannerData[0]} />
+		</>
+	);
+};
+
+export const getServerSideProps = async () => {
+	const q = '*[_type == "product"]';
+	const products = await client.fetch(q);
+
+	const q_banner = '*[_type == "banner"]';
+	const bannerData = await client.fetch(q_banner);
+
+	return {
+		props: {
+			products,
+			bannerData,
+		},
+	};
+};
+
+export default Home;
