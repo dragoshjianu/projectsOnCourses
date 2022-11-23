11b10a48bfafe4ffd1c6c6b975d3711e854bd40d
dragoshjianu <74590358+dragoshjianu@users.noreply.github.com>
2022-11-23 16:30:20 +0200

    updae

--- ecommerce-shop/e-commerce/components/Cart.jsx
+++ ecommerce-shop/e-commerce/components/Cart.jsx
@@ -14,7 +14,8 @@ import { urlFor } from '../lib/client';
 
 const Cart = () => {
 	const cartRef = useRef();
-	const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+	const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity } =
+		useStateContext();
 	return (
 		<div className='cart-wrapper' ref={cartRef}>
 			<div className='cart-container'>
@@ -48,11 +49,17 @@ const Cart = () => {
 									<div className='flex bottom'>
 										<div>
 											<p className='quantity-desc'>
-												<span className='minus' onClick=''>
+												<span
+													className='minus'
+													onClick={() => toggleCartItemQuantity(item._id, 'dec')}
+												>
 													<AiOutlineMinus />
 												</span>
-												<span className='num'>0</span>
-												<span className='plus' onClick=''>
+												<span className='num'>{item.quantity}</span>
+												<span
+													className='plus'
+													onClick={() => toggleCartItemQuantity(item._id, 'inc')}
+												>
 													<AiOutlinePlus />
 												</span>
 											</p>
--- ecommerce-shop/e-commerce/context/StateContext.js
+++ ecommerce-shop/e-commerce/context/StateContext.js
@@ -38,21 +38,35 @@ export const StateContext = ({ children }) => {
 		toast.success(`${qty} ${product.name} added to the cart.`);
 	};
 
-	const increaseQty = () => {
-		setQty((prev) => {
-			return prev + 1;
-		});
-	};
-
 	const toggleCartItemQuantity = (id, value) => {
 		foundProduct = cartItems.find((item) => item._id === id);
 		index = cartItems.findIndex((product) => product._id === id);
 
+		const newCartItems = cartItems.filter((item) => item._id !== id);
+
 		if (value === 'inc') {
-			foundProduct.quantity += 1;
-			cartItems[index] = foundProduct;
+			setCartItems([
+				...newCartItems,
+				{ ...foundProduct, quantity: foundProduct.quantity + 1 },
+			]);
+			setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+			setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
 		} else if (value === 'dec') {
+			if (foundProduct.quantity > 1) {
+				setCartItems([
+					...newCartItems,
+					{ ...foundProduct, quantity: foundProduct.quantity - 1 },
+				]);
+				setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+				setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+			}
 		}
+	};
+
+	const increaseQty = () => {
+		setQty((prev) => {
+			return prev + 1;
+		});
 	};
 
 	const decreaseQty = () => {
@@ -74,6 +88,7 @@ export const StateContext = ({ children }) => {
 				decreaseQty,
 				onAdd,
 				setShowCart,
+				toggleCartItemQuantity,
 			}}
 		>
 			{children}
